37b18d4ff3bfb77e395ffa3072eb2ffd0a2a8e85
Parvin Sattorova <nanajon66@gmail.com>
2024-05-10 14:41:11 -0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import menu from '../assets/open_menu.svg'
+
+export default function Header() {
+    return (
+        <header>
+            <span>
+                Parvin A. Sattorova
+            </span>
+            <button className="menu-btn">
+                <img src={menu} alt='menu' />
+
+            </button>
+
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <a href='#about'>About</a>
+
+                    </li>
+                    <li>
+                        <a href='projects'>Projects</a>
+                    </li>
+                    <li>
+                        <a className='contact'>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import profileImage from '../assets/Dodobashara.JPG'
+
+export default function Hero() {
+  return (
+  <section className='hero-devider'>
+    <div className='image'>
+        <img src={profileImage} alt='Foto of Parvin' />
+    </div>
+  </section>
+  )
+}
