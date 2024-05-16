11bdd92de2033d6363885ed50caed5341e93d5b3
Parvin Sattorova <nanajon66@gmail.com>
2024-05-15 19:11:06 -0600

    Add edits to projects lists

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,16 +28,18 @@ export const projectList = [
     githubLink: 'https://github.com/Sulton88Mehron90/showcase-project',
     image: avicennaMethod,
   },
+
   {
     id: 2,
-    title: 'Rancid Tomatillos',
-    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'Figma', 'GitHub', 'Heroku'],
+    title: 'News Reader',
+    tech: ['React', 'React Router', 'TypeScript', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'API', 'Node.js', 'GitHub'],
     description:
-      'Welcome to Rancid Tomatillos, your go-to platform for all things cinema! Browse our extensive library of movies, delve into detailed information on each film, and even watch trailers. Use the search feature to find movies that pique your interest, or filter films based on ratings to discover hidden gems.',
-    liveLink: 'https://rancid-tomatillos-denzel-parvin.vercel.app/',
-    githubLink: 'https://github.com/Sulton88Mehron90/rancid-tomatillos-denzel-parvin',
-    image: rancidTomatillos,
+      "News Reader is a simple app that lets you catch up on the latest headlines and articles in categories you care about. It aggregates articles from the News API, presenting users with the latest news articles including headlines, images, descriptions, and publishing dates. The app provides a detailed view for each article and a way to filter or search through the news feed.",
+    liveLink: 'https://news-reader-sable.vercel.app/',
+    githubLink: 'https://github.com/Sulton88Mehron90/news-reader',
+    image: newsReader,
   },
+
   {
     id: 3,
     title: 'Refuge - AidConnect/RefugeeAid',
@@ -48,8 +50,31 @@ export const projectList = [
     githubLink: 'https://github.com/Refugee-Aid-Capstone/refugee-aid-fe',
     image: refuge,
   },
+
   {
     id: 4,
+    title: 'Interview Prep Questions',
+    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'API', 'Node.js', 'GitHub'],
+    description:
+      "The Interview Prep Questions App is a dynamic tool designed to assist job seekers, particularly in software development, in preparing for technical and behavioral interviews. It was inspired while getting ready for an interview simulation with an instructor, and built in 7 hours throughout the day. This user-friendly app offers a range of questions and answers, presented in an interactive flashcard format, making your interview preparation engaging and effective.",
+    liveLink: 'https://interview-prep-questions.vercel.app/',
+    githubLink: 'https://github.com/Sulton88Mehron90/interview-prep-questions',
+    image: interviewPrepQuestions,
+  },
+
+  {
+    id: 5,
+    title: 'Rubaiyat of Omar Khayyam.',
+    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'GitHub'],
+    description:
+      "Rubaiyat offers a captivating experience for both poetry enthusiasts and wisdom seekers. Users are presented with random quatrains, merging the age-old insights of Omar Khayyam with the digital age.",
+    liveLink: 'https://rubaiyat.vercel.app/',
+    githubLink: 'https://github.com/Sulton88Mehron90/rubaiyat',
+    image: Rubaiyat,
+  },
+
+  {
+    id: 6,
     title: 'FitChicks',
     tech: ['JavaScript', 'HTML5', 'CSS', 'Webpack', 'Fetch API', 'Figma', 'GitHub', 'Adobe Photoshop'],
     description:
@@ -58,36 +83,18 @@ export const projectList = [
     githubLink: 'https://github.com/Sulton88Mehron90/fitlit-group-project-jy-jm-ps',
     image: fitChicks,
   },
-  {
-    id: 5,
-    title: 'Parks Passport',
-    tech: ['React', 'React Router', 'TypeScript', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'Fetch API', 'Figma', 'GitHub'],
-    description:
-      "Created a web application that allows a user to browse National Parks, look further into details about a chosen park, and view, save, and remove parks in their favorites. Used data from the public NPS API to make a modern and sleek app that displays the parks data in a more pleasing and easily navigated UI. Collaborated within a dynamic team of three to develop a React project, while gaining proficiency in TypeScript, implementing routing with React Router, and performing end-to-end testing using Cypress.",
-    liveLink: 'https://stretch-tech-pi.vercel.app/',
-    githubLink: 'https://github.com/Sulton88Mehron90/stretchTech',
-    image: parkPassport,
-  },
-  {
-    id: 6,
-    title: "What's Cookin'?",
-    tech: ['JavaScript', 'HTML', 'CSS', 'Webpack', 'Mocha', 'Chai', 'Fetch API', 'Figma', 'Git'],
-    description:
-      "What's Cookin'? is a group project completed as a part of Module 2 at Turing School of Software Design's Front-End curriculum. The application hosts a variety of recipes that can be viewed as a whole, by course (salads, hor d'oevres, main courses, sides), or searched by a tag or recipe name to show relevent recipes. When looking at a recipe's page that recipe can be saved to the users saved recipes page. When looking at the saved recipe collection the user may search through their recipes and delete recipes of their choosing.",
-    liveLink: 'https://whats-cookin-group-project.vercel.app/',
-    githubLink: 'https://github.com/JamieCaudill/whats-cookin-group-project',
-    image: whatsCookin,
-  },
+
   {
     id: 7,
-    title: 'ColoRandom',
-    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
+    title: 'Rancid Tomatillos',
+    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'Figma', 'GitHub', 'Heroku'],
     description:
-      "An app was built that displays a randomized palette of five colors. This app allows users to save their favorite palettes to a collection and delete palettes from their saved collection as needed. A web application was developed using vanilla JavaScript to create a randomized color palette and enable users to save and delete previous palettes. Git Workflow practices were utilized for team collaboration, and the project was optimized to use data models to track the user’s saved palettes and the current palette. Feedback from project managers and mentors was incorporated through ongoing code reviews.",
-    liveLink: 'https://andreasorensen.github.io/coloRandom-Team-Project/',
-    githubLink: 'https://github.com/andreasorensen/coloRandom-Team-Project',
-    image: coloRandom,
+      'Welcome to Rancid Tomatillos, your go-to platform for all things cinema! Browse our extensive library of movies, delve into detailed information on each film, and even watch trailers. Use the search feature to find movies that pique your interest, or filter films based on ratings to discover hidden gems.',
+    liveLink: 'https://rancid-tomatillos-denzel-parvin.vercel.app/',
+    githubLink: 'https://github.com/Sulton88Mehron90/rancid-tomatillos-denzel-parvin',
+    image: rancidTomatillos,
   },
+
   {
     id: 8,
     title: 'Travel Tracker',
@@ -98,16 +105,18 @@ export const projectList = [
     githubLink: 'https://github.com/Sulton88Mehron90/travel-tracker',
     image: travelTracker,
   },
+
   {
     id: 9,
-    title: 'Self-Care Center',
-    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
+    title: 'Parks Passport',
+    tech: ['React', 'React Router', 'TypeScript', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'Fetch API', 'Figma', 'GitHub'],
     description:
-      "This is a functional website where you can enter, and choose whether you want to see an affirmation or mantra that can be viewed if you click the 'Receive Message' button to get a random affirmation and mantra from the website. Also if you choose to create your own mantra and affirmation, it is possible to do here! Click the 'Add your Message' button to do that. Your entered affirmation or mantra will be added to the array of affirmation and mantras within the code. When going through the list, your messages will appear from the array list. Working on this project helped not only to gain experience using HTML, CSS, and Javascript. Also, reading the affirmation and mantras helped me a great deal. It truly was God sent, considering the difficult time I am going through in my life right now. I am grateful for the opportunity and the inspiring affirmations and mantras that I've read many times during the project.",
-    liveLink: 'https://sulton88mehron90.github.io/self-care-center/',
-    githubLink: 'https://github.com/Sulton88Mehron90/self-care-center',
-    image: selfCareCenter,
+      "Created a web application that allows a user to browse National Parks, look further into details about a chosen park, and view, save, and remove parks in their favorites. Used data from the public NPS API to make a modern and sleek app that displays the parks data in a more pleasing and easily navigated UI. Collaborated within a dynamic team of three to develop a React project, while gaining proficiency in TypeScript, implementing routing with React Router, and performing end-to-end testing using Cypress.",
+    liveLink: 'https://stretch-tech-pi.vercel.app/',
+    githubLink: 'https://github.com/Sulton88Mehron90/stretchTech',
+    image: parkPassport,
   },
+
   {
     id: 10,
     title: 'Rock-Paper-Scissors',
@@ -118,52 +127,57 @@ export const projectList = [
     githubLink: 'https://github.com/Sulton88Mehron90/Rock-Paper-Scissors',
     image: rockPaperScissors,
   },
+
   {
     id: 11,
     title: 'FlashCards Starter Kit',
     tech: ['JavaScript', 'HTML', 'CSS'],
     description:
-      "This application allows a user to play the game of Rock, Papers, Scissors against the computer who is making random selections. The user can play two different game modes: classic with 3 choices, and advanced with 5 choices. Total wins are saved and displayed for both the user and computer player.",
+      "This project allows users to answer all questions in a set of flash cards using the command line. Users can attempt to answer questions one by one and recieve feedback after each question.",
     liveLink: 'https://github.com/Sulton88Mehron90/flashCards-starter-kit/blob/main/src/parvin.gif',
     githubLink: 'https://github.com/Sulton88Mehron90/flashCards-starter-kit',
     image: flashCards,
   },
+
   {
     id: 12,
-    title: 'Rubaiyat of Omar Khayyam.',
-    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'GitHub'],
+    title: "What's Cookin'?",
+    tech: ['JavaScript', 'HTML', 'CSS', 'Webpack', 'Mocha', 'Chai', 'Fetch API', 'Figma', 'Git'],
     description:
-      "Rubaiyat offers a captivating experience for both poetry enthusiasts and wisdom seekers. Users are presented with random quatrains, merging the age-old insights of Omar Khayyam with the digital age.",
-    liveLink: 'https://rubaiyat.vercel.app/',
-    githubLink: 'https://github.com/Sulton88Mehron90/rubaiyat',
-    image: Rubaiyat,
+      "What's Cookin'? is a group project completed as a part of Module 2 at Turing School of Software Design's Front-End curriculum. The application hosts a variety of recipes that can be viewed as a whole, by course (salads, hor d'oevres, main courses, sides), or searched by a tag or recipe name to show relevent recipes. When looking at a recipe's page that recipe can be saved to the users saved recipes page. When looking at the saved recipe collection the user may search through their recipes and delete recipes of their choosing.",
+    liveLink: 'https://whats-cookin-group-project.vercel.app/',
+    githubLink: 'https://github.com/JamieCaudill/whats-cookin-group-project',
+    image: whatsCookin,
   },
+
   {
     id: 13,
-    title: 'News Reader',
-    tech: ['React', 'React Router', 'TypeScript', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'API', 'Node.js', 'GitHub'],
+    title: 'ColoRandom',
+    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
     description:
-      "News Reader is a simple app that lets you catch up on the latest headlines and articles in categories you care about. It aggregates articles from the News API, presenting users with the latest news articles including headlines, images, descriptions, and publishing dates. The app provides a detailed view for each article and a way to filter or search through the news feed.",
-    liveLink: 'https://news-reader-sable.vercel.app/',
-    githubLink: 'https://github.com/Sulton88Mehron90/news-reader',
-    image: newsReader,
+      "An app was built that displays a randomized palette of five colors. This app allows users to save their favorite palettes to a collection and delete palettes from their saved collection as needed. A web application was developed using vanilla JavaScript to create a randomized color palette and enable users to save and delete previous palettes. Git Workflow practices were utilized for team collaboration, and the project was optimized to use data models to track the user’s saved palettes and the current palette. Feedback from project managers and mentors was incorporated through ongoing code reviews.",
+    liveLink: 'https://andreasorensen.github.io/coloRandom-Team-Project/',
+    githubLink: 'https://github.com/andreasorensen/coloRandom-Team-Project',
+    image: coloRandom,
   },
+
   {
     id: 14,
-    title: 'Interview Prep Questions',
-    tech: ['React', 'React Router', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'API', 'Node.js', 'GitHub'],
+    title: 'Self-Care Center',
+    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
     description:
-      "The Interview Prep Questions App is a dynamic tool designed to assist job seekers, particularly in software development, in preparing for technical and behavioral interviews. It was inspired while getting ready for an interview simulation with an instructor, and built in 7 hours throughout the day. This user-friendly app offers a range of questions and answers, presented in an interactive flashcard format, making your interview preparation engaging and effective.",
-    liveLink: 'https://interview-prep-questions.vercel.app/',
-    githubLink: 'https://github.com/Sulton88Mehron90/interview-prep-questions',
-    image: interviewPrepQuestions,
+      "This is a functional website where you can enter, and choose whether you want to see an affirmation or mantra that can be viewed if you click the 'Receive Message' button to get a random affirmation and mantra from the website. Also if you choose to create your own mantra and affirmation, it is possible to do here! Click the 'Add your Message' button to do that. Your entered affirmation or mantra will be added to the array of affirmation and mantras within the code. When going through the list, your messages will appear from the array list. Working on this project helped not only to gain experience using HTML, CSS, and Javascript. Also, reading the affirmation and mantras helped me a great deal. It truly was God sent, considering the difficult time I am going through in my life right now. I am grateful for the opportunity and the inspiring affirmations and mantras that I've read many times during the project.",
+    liveLink: 'https://sulton88mehron90.github.io/self-care-center/',
+    githubLink: 'https://github.com/Sulton88Mehron90/self-care-center',
+    image: selfCareCenter,
   },
+
   {
     id: 15,
     title: 'Idea Box',
     tech: ['React', 'JavaScript', 'HTML5', 'CSS', 'Cypress', 'Node.js', 'GitHub'],
     description:
-      "The Interview Prep Questions App is a dynamic tool designed to assist job seekers, particularly in software development, in preparing for technical and behavioral interviews. It was inspired while getting ready for an interview simulation with an instructor, and built in 7 hours throughout the day. This user-friendly app offers a range of questions and answers, presented in an interactive flashcard format, making your interview preparation engaging and effective.",
+      "",
     liveLink: 'https://idea-box-one-react.vercel.app/',
     githubLink: 'https://github.com/Sulton88Mehron90/idea-box-one-react',
     image: ideaBox,
