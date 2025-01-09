d0206af764c751a88ddb692fbe10d526413b7cff
Parvin Sattorova <nanajon66@gmail.com>
2025-01-09 15:52:24 -0700

    feat: fix social media icons to open links in new tabs and link contact form page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,139 +1,5 @@
   // https://formspree.io/f/mqkrjwek
   
-import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Icons } from './Icons';
-import '../components/ContactForm.css';
-import '../index.css';
-
-const ContactForm = () => {
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  
-  const [formData, setFormData] = useState({
-    email: '',
-    name: '',
-    message: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('https://formspree.io/f/mqkrjwek', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(response => {
-      if (response.ok) {
-        toast.success('Message sent successfully!', {
-          position: "bottom-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          style: {
-            backgroundColor: 'var(--primary)',
-            color: 'var(--white)',
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-          }
-        });
-        setFormData({ email: '', name: '', message: '' });
-      } else {
-        toast.error('Failed to send message.', {
-          position: "bottom-center",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          style: {
-            backgroundColor: 'var(--primary)',
-            color: 'var(--white)',
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-          }
-        });
-      }
-    });
-  };
-
-  return (
-    <div className="contact-form-container">
-      <div className="contact-form">
-        <h2>Feel free to get in touch with me. Thank you!</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Your Email Address *</label>
-          <input 
-            type="email" 
-            id="email" 
-            name="email" 
-            value={formData.email} 
-            onChange={handleChange} 
-            required 
-          />
-
-          <label htmlFor="name">Your Name *</label>
-          <input 
-            type="text" 
-            id="name" 
-            name="name" 
-            value={formData.name} 
-            onChange={handleChange} 
-            required 
-          />
-
-          <label htmlFor="message">Message *</label>
-          <textarea 
-            id="message" 
-            name="message" 
-            rows="4" 
-            value={formData.message} 
-            onChange={handleChange} 
-            required 
-          ></textarea>
-
-          <button type="submit">Send Message</button>
-        </form>
-        <ToastContainer />
-      </div>
-      <div className="socials-container">
-        <a href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/">
-          <Icons.Linkedin />
-        </a>
-        <a href="https://github.com/Sulton88Mehron90">
-          <Icons.Github />
-        </a>
-        <a href="https://www.instagram.com/sulimesho/">
-          <Icons.Instagram />
-        </a>
-        <a href="https://twitter.com/nanajon66">
-          <Icons.Twitter />
-        </a>
-        <a href="https://www.facebook.com/parvin.sattorova/">
-          <Icons.Facebook />
-        </a>
-      </div>
-    </div>
-  );
-};
-
-export default ContactForm;
-
 // import React, { useState, useEffect } from 'react';
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
@@ -142,10 +8,11 @@ export default ContactForm;
 // import '../index.css';
 
 // const ContactForm = () => {
+
 //   useEffect(() => {
 //     window.scrollTo(0, 0);
 //   }, []);
-
+  
 //   const [formData, setFormData] = useState({
 //     email: '',
 //     name: '',
@@ -169,40 +36,40 @@ export default ContactForm;
 //       },
 //       body: JSON.stringify(formData)
 //     })
-//       .then(response => {
-//         if (response.ok) {
-//           toast.success('Message sent successfully!', {
-//             position: 'bottom-right',
-//             autoClose: 3000,
-//             hideProgressBar: false,
-//             closeOnClick: true,
-//             pauseOnHover: true,
-//             draggable: true,
-//             progress: undefined,
-//             style: {
-//               backgroundColor: 'var(--primary)',
-//               color: 'var(--white)',
-//               boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-//             }
-//           });
-//           setFormData({ email: '', name: '', message: '' });
-//         } else {
-//           toast.error('Failed to send message.', {
-//             position: 'bottom-center',
-//             autoClose: 3000,
-//             hideProgressBar: false,
-//             closeOnClick: true,
-//             pauseOnHover: true,
-//             draggable: true,
-//             progress: undefined,
-//             style: {
-//               backgroundColor: 'var(--primary)',
-//               color: 'var(--white)',
-//               boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-//             }
-//           });
-//         }
-//       });
+//     .then(response => {
+//       if (response.ok) {
+//         toast.success('Message sent successfully!', {
+//           position: "bottom-right",
+//           autoClose: 3000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//           style: {
+//             backgroundColor: 'var(--primary)',
+//             color: 'var(--white)',
+//             boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+//           }
+//         });
+//         setFormData({ email: '', name: '', message: '' });
+//       } else {
+//         toast.error('Failed to send message.', {
+//           position: "bottom-center",
+//           autoClose: 3000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//           style: {
+//             backgroundColor: 'var(--primary)',
+//             color: 'var(--white)',
+//             boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+//           }
+//         });
+//       }
+//     });
 //   };
 
 //   return (
@@ -211,33 +78,33 @@ export default ContactForm;
 //         <h2>Feel free to get in touch with me. Thank you!</h2>
 //         <form onSubmit={handleSubmit}>
 //           <label htmlFor="email">Your Email Address *</label>
-//           <input
-//             type="email"
-//             id="email"
-//             name="email"
-//             value={formData.email}
-//             onChange={handleChange}
-//             required
+//           <input 
+//             type="email" 
+//             id="email" 
+//             name="email" 
+//             value={formData.email} 
+//             onChange={handleChange} 
+//             required 
 //           />
 
 //           <label htmlFor="name">Your Name *</label>
-//           <input
-//             type="text"
-//             id="name"
-//             name="name"
-//             value={formData.name}
-//             onChange={handleChange}
-//             required
+//           <input 
+//             type="text" 
+//             id="name" 
+//             name="name" 
+//             value={formData.name} 
+//             onChange={handleChange} 
+//             required 
 //           />
 
 //           <label htmlFor="message">Message *</label>
-//           <textarea
-//             id="message"
-//             name="message"
-//             rows="4"
-//             value={formData.message}
-//             onChange={handleChange}
-//             required
+//           <textarea 
+//             id="message" 
+//             name="message" 
+//             rows="4" 
+//             value={formData.message} 
+//             onChange={handleChange} 
+//             required 
 //           ></textarea>
 
 //           <button type="submit">Send Message</button>
@@ -245,39 +112,19 @@ export default ContactForm;
 //         <ToastContainer />
 //       </div>
 //       <div className="socials-container">
-//         <a
-//           href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
+//         <a href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/">
 //           <Icons.Linkedin />
 //         </a>
-//         <a
-//           href="https://github.com/Sulton88Mehron90"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
+//         <a href="https://github.com/Sulton88Mehron90">
 //           <Icons.Github />
 //         </a>
-//         <a
-//           href="https://www.instagram.com/sulimesho/"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
+//         <a href="https://www.instagram.com/sulimesho/">
 //           <Icons.Instagram />
 //         </a>
-//         <a
-//           href="https://twitter.com/nanajon66"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
+//         <a href="https://twitter.com/nanajon66">
 //           <Icons.Twitter />
 //         </a>
-//         <a
-//           href="https://www.facebook.com/parvin.sattorova/"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
+//         <a href="https://www.facebook.com/parvin.sattorova/">
 //           <Icons.Facebook />
 //         </a>
 //       </div>
@@ -286,3 +133,156 @@ export default ContactForm;
 // };
 
 // export default ContactForm;
+
+import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Icons } from './Icons';
+import '../components/ContactForm.css';
+import '../index.css';
+
+const ContactForm = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://formspree.io/f/mqkrjwek', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => {
+        if (response.ok) {
+          toast.success('Message sent successfully!', {
+            position: 'bottom-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            style: {
+              backgroundColor: 'var(--primary)',
+              color: 'var(--white)',
+              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+            }
+          });
+          setFormData({ email: '', name: '', message: '' });
+        } else {
+          toast.error('Failed to send message.', {
+            position: 'bottom-center',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            style: {
+              backgroundColor: 'var(--primary)',
+              color: 'var(--white)',
+              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+            }
+          });
+        }
+      });
+  };
+
+  return (
+    <div className="contact-form-container">
+      <div className="contact-form">
+        <h2>Feel free to get in touch with me. Thank you!</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Your Email Address *</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="name">Your Name *</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="message">Message *</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+
+          <button type="submit">Send Message</button>
+        </form>
+        <ToastContainer />
+      </div>
+      <div className="socials-container">
+        <a
+          href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icons.Linkedin />
+        </a>
+        <a
+          href="https://github.com/Sulton88Mehron90"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icons.Github />
+        </a>
+        <a
+          href="https://www.instagram.com/sulimesho/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icons.Instagram />
+        </a>
+        <a
+          href="https://twitter.com/nanajon66"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icons.Twitter />
+        </a>
+        <a
+          href="https://www.facebook.com/parvin.sattorova/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icons.Facebook />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
