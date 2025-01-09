7b1d50da76282a9e53b9e90f921ccfbe8e40bd49
Parvin Sattorova <nanajon66@gmail.com>
2025-01-09 15:50:03 -0700

    feat: fix social media icons to open links in new tabs and link contact form page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,9 @@
+// 
+
+
 import { Icons } from './Icons';
-import {Link } from 'react-router-dom'
-import '../index.css'
+import { Link } from 'react-router-dom';
+import '../index.css';
 
 const Contact = () => {
   return (
@@ -8,26 +11,52 @@ const Contact = () => {
       <h2>Contact</h2>
       <p>Let&apos;s connect and create together.</p>
       <div className="socials-container">
-        <a href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/">
+        <a
+          href="https://www.linkedin.com/in/parvin-sattorova-edwards-357526b3/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
           <Icons.Linkedin />
         </a>
-        <a href="https://github.com/Sulton88Mehron90">
+        <a
+          href="https://github.com/Sulton88Mehron90"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
           <Icons.Github />
         </a>
-        <a href="https://www.instagram.com/sulimesho/">
+        <a
+          href="https://www.instagram.com/sulimesho/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
           <Icons.Instagram />
         </a>
-        <a href="https://twitter.com/nanajon66">
+        <a
+          href="https://twitter.com/nanajon66"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
           <Icons.Twitter />
         </a>
-        <a href="https://www.facebook.com/parvin.sattorova/">
+        <a
+          href="https://www.facebook.com/parvin.sattorova/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+        >
           <Icons.Facebook />
         </a>
       </div>
-      <Link to="/contact-form" className="send-message">Message me ...</Link>
+      <Link to="/contact-form" className="send-message">
+        Message me ...
+      </Link>
     </section>
   );
 };
 
 export default Contact;
-
